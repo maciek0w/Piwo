d8b30e4e65edfa1ce82a86a4596f22e377f879eb
LetorMerlin <maciek200191@gmail.com>
2022-03-11 13:44:56 +0100

    JD

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Navbar(){
+    return <nav className="navbar bg-dark container">
+        <h3>Home</h3>
+        <h3>Ranking</h3>
+    </nav>
+}
+export default Navbar;
