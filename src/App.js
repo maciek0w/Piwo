e3e7396e5b1c0fd6552cea91f7ce79cc4a4fa96c
LetorMerlin <maciek200191@gmail.com>
2022-03-11 17:20:11 +0100

    maciek

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {} from "react-router-dom";
-import Navbar from './component/navbar';
+import Navbar from './component/Navbar';
 
 function App() {
   return <Navbar />
